816723379f9dc5c64f353ff666f482ea0f1d3067
Hardik Ghoshal <hardik@Hardiks-MacBook-Air.local>
2024-05-23 22:33:31 +0200

    working O(n^2logn) solution, takes too much time for larger input size

--- src/eliminate.rs
+++ src/eliminate.rs
@@ -1,29 +1,29 @@
-use std::collections::{HashSet,BinaryHeap};
+use std::collections::HashSet;
 use std::cmp::Reverse;
 use priority_queue::PriorityQueue;
 
 use crate::print;
-pub fn eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, row: usize, col: usize, num: usize, min: &mut usize, rmin: &mut usize, cmin: &mut usize, n_ones: &mut usize, min_heap: &mut BinaryHeap<Reverse<(usize, usize, usize)>>, pq:&mut PriorityQueue<(usize,usize), usize>  ) -> bool {
+pub fn eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, row: usize, col: usize, num: usize, n_ones: &mut usize, pq:&mut PriorityQueue<(usize,usize), Reverse<usize> >, invalid: &mut bool ) -> bool {
+    
+    if *invalid {
+        return false;
+    }
+    
     let mut changed = false;
+
     if puzzle[row][col].len() != 1 {
-        print!("Error: Trying to eliminate from a cell with more than one value");
+        // print!("Error: Trying to eliminate from a cell with more than one value");
         return false;
     }
     //row
     for i in 0..puzzle.len() {
-        // if puzzle[row][i].contains(&num) && puzzle[row][i].len() > 1 && i != col{
         if puzzle[row][i].contains(&num) && i != col{    
             puzzle[row][i].remove(&num);
-            println!("row: row ={} i={} len(row,i)={}, num removed={}", row, i, puzzle[row][i].len(), num);
             if puzzle[row][i].len() == 0 {
-                *min = 0;
-                *rmin = row;
-                *cmin = i;
-            }
-            if puzzle[row][i].len() > 1 && *min > puzzle[row][i].len() {
-                *rmin = row;
-                *cmin = i;
-                *min = puzzle[row][i].len();
+                // println!("Error: Empty set in row col {},{}", row, i);
+                pq.push((row, i), Reverse(0)); // (key, priority)
+                *invalid = true;
+                return false;
             }
             changed = true;
 
@@ -31,50 +31,52 @@ pub fn eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, row: usize, col: usize, 
                 *n_ones += 1;
                 let nums: Vec<usize> = puzzle[row][i].iter().cloned().collect();
                 for num in nums{
-                    println!("Just before the recursive call row");
-                    if eliminate(puzzle, row, i, num, min, rmin, cmin, n_ones, min_heap, pq) {
+                    // println!("Just before the recursive call row");
+                    // pq.change_priority(&(row,i), Reverse(usize::MAX));
+                    pq.remove(&(row, i));
+                    if eliminate(puzzle, row, i, num,  n_ones, pq, invalid) {
                         changed = true;
+                    } else{
+                        changed = false;
                     }
                 }
-            }
-            if puzzle[row][i].len() != 1 {
-                min_heap.push(Reverse((puzzle[row][i].len(), row, i)));
+            }else{
+                pq.push((row, i), Reverse(puzzle[row][i].len()));
             }
         }
-        pq.push((row, i), puzzle[row][i].len());
+        if puzzle[row][i].len() == 0 {
+            // println!("Error: Empty set in row col {},{}", row, i);
+        }
     }
     //col
     for i in 0..puzzle.len() {
-        // if puzzle[i][col].contains(&num) && puzzle[i][col].len() > 1 && i != row{
             if puzzle[i][col].contains(&num) && i != row{
                 puzzle[i][col].remove(&num);
+                changed = true;
                 if puzzle[i][col].len() == 0 {
-                    *min = 0;
-                    *rmin = i;
-                    *cmin = col;
+                    // println!("Error: Empty set in row col {},{}", i, col);
+                    pq.push((i, col), Reverse(0)); // (key, priority)
+                    *invalid = true;
+                    return false;
                 }
-                if puzzle[i][col].len() > 1 && *min > puzzle[i][col].len() {
-                    println!("col: i ={} col={} len(i,col)={}, num removed={}", i, col, puzzle[i][col].len(), num);
-                    *rmin = row;
-                    *cmin = i;
-                    *min = puzzle[i][col].len();
-                }
-            changed = true;
-            if puzzle[i][col].len() == 1 {
-                *n_ones += 1;
-                let nums: Vec<usize> = puzzle[i][col].iter().cloned().collect();
-                for num in nums{
-                    println!("Just before the recursive call col");
-                    if eliminate(puzzle, i, col, num, min, rmin, cmin, n_ones, min_heap, pq) {
-                        changed = true;
+                if puzzle[i][col].len() == 1 {
+                    *n_ones += 1;
+                    let nums: Vec<usize> = puzzle[i][col].iter().cloned().collect();
+                    for num in nums{
+                        pq.remove(&(i, col));
+                        if eliminate(puzzle, i, col, num, n_ones, pq, invalid) {
+                            changed = true;
+                        }else{
+                            changed = false;
+                        }
                     }
+                }else{
+                    pq.push((i, col), Reverse(puzzle[i][col].len()));
                 }
-            }
-            if puzzle[i][col].len() != 1 {
-                min_heap.push(Reverse((puzzle[i][col].len(), i, col)));
-            }
         }
-        pq.push((i, col), puzzle[i][col].len());
+        if puzzle[i][col].len() == 0 {
+            // println!("Error: Empty set in row col {},{}", i, col);
+        }
     }
     let size = (puzzle.len() as f64).sqrt() as usize;
     let start_row = row - row % size;
@@ -82,87 +84,34 @@ pub fn eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, row: usize, col: usize, 
     //box
     for i in 0..size {
         for j in 0..size {
-            // if puzzle[i + start_row][j + start_col].contains(&num) && puzzle[i + start_row][j + start_col].len() > 1 && (i + start_row != row || j + start_col != col){
                 if puzzle[i + start_row][j + start_col].contains(&num) && (i + start_row != row || j + start_col != col){
-                    // println!("Eliminating {} from ({}, {}), set contents: {:?}", num, i + start_row, j + start_col, puzzle[i + start_row][j + start_col]);
                     puzzle[i + start_row][j + start_col].remove(&num);
-
+                    changed = true;
                     if puzzle[i + start_row][j + start_col].len() == 0 {
-                        *min = 0;
-                        *rmin = i + start_row;
-                        *cmin = j + start_col;
+                        // println!("Error: Empty set in row col {},{}", i + start_row, j + start_col);
+                        pq.push((i + start_row, j + start_col), Reverse(0)); // (key, priority)
+                        *invalid = true;
+                        return false;
                     }
-
-                    if puzzle[i + start_row][j + start_col].len() > 1 && *min > puzzle[i + start_row][j + start_col].len(){
-                        println!("box: i ={} j={} len(i,j)={} num removed={}", i + start_row, j + start_col, puzzle[i + start_row][j + start_col].len(), num);
-                        *rmin = i + start_row;
-                        *cmin = j + start_col;
-                        *min = puzzle[i + start_row][j + start_col].len();
-                    }
-                    // let x = puzzle[i + start_row][j + start_col].len();
-                // if *min > x {
-                //     *rmin = i + start_row;
-                //     *cmin = j + start_col;
-                //     *min = x;
-                // }
-                changed = true;
-                if puzzle[i + start_row][j + start_col].len() == 1 {
-                    *n_ones += 1;
-                    let nums: Vec<usize> = puzzle[i + start_row][j + start_col].iter().cloned().collect();
-                    for num in nums{
-                        println!("Just before the recursive call box");
-                        if eliminate(puzzle, i + start_row, j + start_col, num, min, rmin, cmin, n_ones, min_heap, pq) {
-                            changed = true;
+                    if puzzle[i + start_row][j + start_col].len() == 1 {
+                        *n_ones += 1;
+                        let nums: Vec<usize> = puzzle[i + start_row][j + start_col].iter().cloned().collect();
+                        for num in nums{
+                            pq.remove(&(i + start_row, j + start_col));
+                            if eliminate(puzzle, i + start_row, j + start_col, num, n_ones, pq, invalid) {
+                                changed = true;
+                            } else{
+                                changed = false;
+                            }
                         }
+                    }else{
+                        pq.push((i + start_row, j + start_col), Reverse(puzzle[i + start_row][j + start_col].len()));
                     }
-                }
-                if puzzle[i + start_row][j + start_col].len() != 1 {
-                    min_heap.push(Reverse((puzzle[i + start_row][j + start_col].len(), i + start_row, j + start_col)));
-                }
             }
-            pq.push((i + start_row, j + start_col), puzzle[i + start_row][j + start_col].len());
+            if puzzle[i + start_row][j + start_col].len() == 0 {
+                // println!("Error: Empty set in row col {},{}", i + start_row, j + start_col);
+            }
         }
     }
     return changed;
 }
-
-// use std::collections::HashSet;
-// use rayon::prelude::*;
-
-// pub fn eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, row: usize, col: usize, num: usize) -> &mut Vec<Vec<HashSet<usize>>> {
-//   let size = puzzle.len();
-//   let sqrt_size = (size as f64).sqrt() as usize;
-//   let start_row = row - row % sqrt_size;
-//   let start_col = col - col % sqrt_size;
-
-//   // Check the row
-//   if puzzle[row].par_iter().any(|x| x.contains(&num)) {
-//      puzzle[row].par_iter_mut().for_each(|x| {
-//      if x.len() > 1{
-//        x.remove(&num);
-//      }
-//      });
-//   }
-
-//   // Check the column
-//   if puzzle.par_iter().any(|x| x[col].contains(&num)) {
-//     puzzle.par_iter_mut().for_each(|x| {
-//     if x[col].len() > 1 {
-//       x[col].remove(&num);
-//     }
-//     });
-//   }
-
-//   // Check the box
-//   for i in start_row..start_row + sqrt_size {
-//     if puzzle[i][start_col..start_col + sqrt_size].par_iter().any(|x| x.contains(&num)) {
-//       puzzle[i][start_col..start_col + sqrt_size].par_iter_mut().for_each(|x| {
-//         if x.len() > 1 {
-//           x.remove(&num);
-//         }
-//       });
-//     }
-//   }
-
-//   return puzzle;
-// }
--- src/find_and_eliminate.rs
+++ src/find_and_eliminate.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet,BinaryHeap};
+use std::collections::HashSet;
 use priority_queue::PriorityQueue;
 use std::cmp::Reverse;
 use crate::eliminate;
@@ -13,24 +13,21 @@ lazy_static! {
 }
 
 
-fn _eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, min_val: &mut usize, rmin: &mut usize, cmin: &mut usize, n_ones: &mut usize,  min_heap: &mut BinaryHeap<Reverse<(usize, usize, usize)>>, pq:&mut PriorityQueue<(usize,usize), usize> ) -> bool {
+fn _eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>, n_ones: &mut usize, pq:&mut PriorityQueue<(usize,usize), Reverse<usize>>, invalid: &mut bool ) -> bool {
     let mut changed = false;
     for i in 0..puzzle.len(){
         for j in 0..puzzle.len(){ 
             if puzzle[i][j].len() == 1{
                 let nums: Vec<usize> = puzzle[i][j].iter().cloned().collect();
                 for num in nums{
-                    if eliminate::eliminate(puzzle, i, j, num, min_val, rmin, cmin,  n_ones, min_heap, pq) {
+                    if eliminate::eliminate(puzzle, i, j, num,  n_ones, pq, invalid) {
                         changed = true;
                     }
-                    println!("_eliminate rmin = {}, cmin = {}, min_val = {}, n_ones = {}, num = {}, i={}, j={}", rmin, cmin, min_val, n_ones, num, i, j);
                 }
             }
         }
-        println!("");
+        // println!("");
     }
-    println!("\n%%%%%%%\n");
-    print::print_puzzle(puzzle);
     return changed;
 }
 
@@ -75,78 +72,91 @@ fn is_valid_cell(puzzle: &Vec<Vec<HashSet<usize>>>, row: usize, col: usize) -> b
 }
 
 pub fn search(puzzle: &mut Vec<Vec<HashSet<usize>>>) {
-    let mut rmin_old = 0;
-    let mut cmin_old = 0;
-    let mut min_val_old = usize::MAX;
+    let mut invalid: bool = false;
+    let size = puzzle.len()*puzzle.len();
     let mut n_ones_old = 0;
-    let mut min_heap: BinaryHeap<Reverse<(usize, usize, usize)>> = BinaryHeap::new();
-    let mut pq: PriorityQueue<(usize,usize), usize> = priority_queue::PriorityQueue::new();
+    let mut pq: PriorityQueue<(usize,usize), Reverse<usize>> = priority_queue::PriorityQueue::new();
     for i in 0..puzzle.len(){
         for j in 0..puzzle.len(){
-            min_heap.push(Reverse((puzzle[i][j].len(), i, j)));
             if puzzle[i][j].len() == 1{
                 n_ones_old += 1;
             }
-            // pq.push((i,j), puzzle[i][j].len());
         }
     }
-    // pq.push((8,7), 5);
-    while _eliminate(puzzle, &mut min_val_old, &mut rmin_old, &mut cmin_old, &mut n_ones_old, &mut min_heap, &mut pq) {println!("Eliminating");}
-    for (item, priority) in pq.iter() {
-        println!("item = {:?}, priority = {}", item, priority);
-    }
+    while _eliminate(puzzle,  &mut n_ones_old, &mut pq, &mut invalid) {println!("Eliminating");}
     print::print_puzzle(&puzzle);
-    let mut stack: Vec<(Vec<Vec<HashSet<usize>>>, (usize,usize,usize,usize), BinaryHeap<Reverse<(usize,usize,usize)>>)> = Vec::new();
-    stack.push( ( puzzle.clone(), (rmin_old, cmin_old, min_val_old, n_ones_old), min_heap));
-    println!("Stack length = {}, rmin={}, cmin={}, min_val={}, n_ones={}", stack.len(), rmin_old, cmin_old, min_val_old, n_ones_old);
+
+    //context.0 = puzzle, context.1 = pq, context.2 = n_ones
+    let mut stack: Vec<( Vec<Vec<HashSet<usize>>>, PriorityQueue<(usize,usize), Reverse<usize>>, usize )> = Vec::new();
+
+    stack.push( ( puzzle.clone(), pq , n_ones_old) );
     while !stack.is_empty() {
+
         let context = stack.pop().unwrap();
         let puzzle = context.0;
-        let (mut rmin, mut cmin, mut min_val, mut n_ones) = context.1;
-        let mut min_heap = context.2;
-        let (minimum, row_min, col_min) = min_heap.pop().unwrap().0;
-        // println!("rmin = {}, cmin = {}, min_val = {}, n_ones = {}", rmin, cmin, min_val, n_ones);
-        // let mut puzzle = stack.pop().unwrap();
-        // let changed = _eliminate(&mut puzzle);
-        // print::print_puzzle(&puzzle);
-        // let (row, col, min) = mrv::mrv(&mut puzzle);
+        let mut pq = context.1;
+        let mut n_ones = context.2;
 
-        if minimum == 0{
-            println!("Invalid config reached");
-            continue; // invalid config reached
-        }
-        // eliminate::eliminate(&mut puzzle, row, col);
-        if n_ones == puzzle.len() * puzzle.len() {
-            println!("Found a solution!");
+        if n_ones == size{
+            println!("Found a solution! #ones = {}", n_ones);
             let mut n_solutions = N_SOLUTIONS.lock().unwrap();
             *n_solutions += 1;
             print::print_puzzle(&puzzle);
             continue;
         }
-        // println!("got here");
-        let possibilities = &puzzle[row_min][col_min];
-        if possibilities.len() == 1 {
-            println!("possibilities = 1, invalid");
-            println!("puzzle[{}][{}] = {:?}",row_min, col_min, puzzle[row_min][col_min]);
+        let row_min ;
+        let col_min ;
+        // let minimum = pq.pop().unwrap();
+        if let Some((key, Reverse(priority))) = pq.pop() {
+            // Now you can use `key` and `priority`
+            if priority == 0 {
+                // println!("Invalid config reached priority = 0");
+                continue; // invalid config reached
+            }
+            if priority == usize::MAX {
+                // println!("Invalid config reached priority = MAX");
+                continue; // invalid config reached
+            } else{
+                row_min = key.0;
+                col_min = key.1;
+                // println!("PRIORITY = {}", priority);
+            }
+        } else {
+            // println!("Priority queue is empty");
             continue;
         }
-        println!("possibilities = {:?}", possibilities);
+        
+        let possibilities = &puzzle[row_min][col_min];
+
+        if possibilities.len() == 1 {
+            // println!("possibilities = 1, invalid");
+            // println!("puzzle[{}][{}] = {:?}",row_min, col_min, puzzle[row_min][col_min]);
+            continue;
+        }
+
+        // println!("possibilities = {:?}", possibilities);
         for num in possibilities{
             let mut new_puzzle = puzzle.clone();
-            let mut new_min_heap = min_heap.clone();
-            let n_ones_new = n_ones.clone();
-
+            let mut n_ones_new = n_ones.clone();
+            let mut new_pq = pq.clone();
+            n_ones_new += 1;
+            if n_ones_new == size{
+                // println!("Found a solution! #ones = {}", n_ones_new);
+                let mut n_solutions = N_SOLUTIONS.lock().unwrap();
+                *n_solutions += 1;
+                print::print_puzzle(&new_puzzle);
+                continue;
+            }    
+            // println!("Trying to insert {} at puzzle[{}][{}], #ones={}", num, row_min, col_min, n_ones_new);
             new_puzzle[row_min][col_min] = HashSet::new();
             new_puzzle[row_min][col_min].insert(*num);
-            // println!("Trying to insert {} at ({}, {}), puzzle[{}][{}]={:?}", num, rmin, cmin, rmin, cmin, new_puzzle[rmin][cmin] );
-            // while _eliminate(&mut new_puzzle, &mut min_val, &mut rmin, &mut cmin, &mut n_ones) {println!("Eliminating");}
-            let test = eliminate::eliminate(&mut new_puzzle, row_min, col_min, *num, &mut min_val, &mut rmin, &mut cmin, &mut n_ones, &mut new_min_heap, &mut pq.clone());
-            // println!("rmin = {} cmin= {}, min = {}, n_ones = {}, num= {}", rmin, cmin, min_val, n_ones, num);
-            // println!("test = {}", test);
-            // println!("rmin = {}, cmin = {}, min_val = {}, n_ones = {}", rmin, cmin, min_val, n_ones);
-            // while _eliminate(&mut new_puzzle) {}
-            stack.push((new_puzzle, (row_min, col_min, min_val, n_ones_new), new_min_heap ));
-            // println!("Stack length = {}", stack.len());
+            eliminate::eliminate(&mut new_puzzle, row_min, col_min, *num, &mut n_ones_new, &mut new_pq, &mut invalid);
+            if invalid {
+                // println!("Invalid config reached invalid = true");
+                invalid = false;
+                continue;
+            }
+            stack.push((new_puzzle, new_pq, n_ones_new ));
         }
     }
 }
@@ -155,4 +165,3 @@ pub fn find_and_eliminate(puzzle: &mut Vec<Vec<HashSet<usize>>>){
     search(puzzle);
     println!("num_solutions = {}",N_SOLUTIONS.lock().unwrap());
 }
-
